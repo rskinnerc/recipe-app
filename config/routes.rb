e7289218f12dba297595aa59669cea5318ec14d9
Ronald <rjskinnerc@unal.edu.co>
2022-08-30 16:26:53 -0500

    Add public recipes feature with tdd

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # root to: "home#index"
   resources :recipes, only: [:index, :show, :new, :create, :destroy]
+
+  resources :public_recipes, only: [:index]
 end
---
+++ spec/requests/public_recipes_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "PublicRecipes", type: :request do
+  before(:all) do
+    @user = User.create!(name: 'Test User 3', email: 'test3@email.com', password: 'password')
+    @first = Recipe.create!(name: 'Test Recipe 1', description: 'Test Description', public: false, cooking_time: 60,
+                            preparation_time: 60, user: @user)
+    @second = Recipe.create!(name: 'Test Recipe 2', description: 'Test Description', public: true, cooking_time: 60,
+                             preparation_time: 60, user: @user)
+    Recipe.create!(name: 'Test Recipe 3', description: 'Test Description', public: true, cooking_time: 60,
+                   preparation_time: 60, user: @user)
+    Recipe.create!(name: 'Test Recipe 4', description: 'Test Description', public: false, cooking_time: 60,
+                   preparation_time: 60, user: @user)
+  end
+  describe "GET /" do
+    it "returns http success" do
+      get public_recipes_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      get public_recipes_path
+      expect(response).to render_template('index')
+    end
+
+    it "should display all public recipes from newest to oldest" do
+      get public_recipes_path
+      expect(response.body).to include('Test Recipe 3')
+      expect(response.body).to include('Test Recipe 2')
+      expect(response.body).to_not include('Test Recipe 1')
+      expect(response.body).to_not include('Test Recipe 4')
+    end
+  end
+end
