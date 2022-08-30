d9860c797c910ee426aee446d6194c8e5bcee700
Ronald <rjskinnerc@unal.edu.co>
2022-08-29 19:24:44 -0500

    Add recipe model and tests

---
+++ app/models/recipe.rb
@@ -0,0 +1,6 @@
+class Recipe < ApplicationRecord
+  belongs_to :user
+
+  validates :name, :preparation_time, :cooking_time, :public, :description, presence: true
+  validates :preparation_time, :cooking_time, numericality: { greater_than_or_equal_to: 0 }
+end
