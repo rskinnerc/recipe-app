285eee390d0da664a789dda8f28d1ecd656443cf
saifullah767 <skj99667788@gmail.com>
2022-09-01 00:31:46 +0500

    ref created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_212143) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_191851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_212143) do
     t.integer "quantity", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -48,5 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_212143) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
   add_foreign_key "recipes", "users"
 end
