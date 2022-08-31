11382e5b7a6461fcbba3015d77faf40a6ae865bc
Ronald <rjskinnerc@unal.edu.co>
2022-08-31 15:00:40 -0500

    Add logo and style nav menu
    - Fixed request specs to handle authentiation guard

--- spec/features/navigation_menu_spec.rb
+++ spec/features/navigation_menu_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'NavigationMenus', type: :feature do
   scenario 'Not authenticated users can see the navigation menu with public links' do
     visit root_path
     expect(page).to have_link 'Public Recipes'
-    expect(page).to have_link 'Recipes App'
+    expect(page).to have_selector '#logo'
     expect(page).to have_link 'Login'
     expect(page).to have_link 'Sign Up'
   end
@@ -14,7 +14,7 @@ RSpec.feature 'NavigationMenus', type: :feature do
     sign_in user
     visit root_path
     expect(page).to have_link 'Public Recipes'
-    expect(page).to have_link 'Recipes App'
+    expect(page).to have_selector '#logo'
     expect(page).to have_link 'My Recipes'
     expect(page).to_not have_link 'Login'
   end
