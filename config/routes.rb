778d62022c18e89ada642b2004b1a783c6b4fb8a
Ronald <rjskinnerc@unal.edu.co>
2022-09-02 11:43:25 -0500

    Style forms and add shopping list feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
-
+  
   root to: "recipes#index"
+  get 'shopping_list', to: 'shopping_list#index'
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
     resources :ingredients, only: [:new, :create, :destroy]
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    expect(User.new(email: 'some@user.com', password: 'password')).to be_valid
+  end
 end
