0a87dff4280150d93e90e08317c57002a3e81ffa
Ronald <rjskinnerc@unal.edu.co>
2022-09-02 11:04:03 -0500

    Implement public recipes view and total price

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,8 @@ class Recipe < ApplicationRecord
 
   validates :name, :preparation_time, :cooking_time, :description, presence: true
   validates :preparation_time, :cooking_time, numericality: { greater_than_or_equal_to: 0 }
+
+  def total_price
+    recipe_foods.includes(:food).map { |recipe_food| recipe_food.food.price * recipe_food.quantity }.sum
+  end
 end
